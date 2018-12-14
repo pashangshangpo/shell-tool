033bdfcd3afd5a6ab4363e74278fe6326c512847
爬上上坡 <pashangshangpo@gmail.com>
2018-12-14 21:17:15 +0800

    chore: 更新打包

--- dist/shell-tool.cjs.js
+++ dist/shell-tool.cjs.js
@@ -5,17 +5,17 @@ Object.defineProperty(exports, '__esModule', { value: true });
 function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
 
 var Shell = _interopDefault(require('shelljs'));
+var _regeneratorRuntime = _interopDefault(require('@babel/runtime/regenerator'));
+var _objectSpread = _interopDefault(require('@babel/runtime/helpers/objectSpread'));
+var _asyncToGenerator = _interopDefault(require('@babel/runtime/helpers/asyncToGenerator'));
+var path = require('path');
+var nodeTool = require('node-tool');
 
-/**
- * @file shell
- * @author xiaozhihua
- * @date 2018-12-12 09:13:49
- */
-var Exec = function Exec(shell) {
+var Exec = function Exec(shell, options) {
   return new Promise(function (resolve) {
-    Shell.exec(shell, {
+    Shell.exec(shell, _objectSpread({
       silent: true
-    }, function (code) {
+    }, options), function (code) {
       var output = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '';
       var err = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : '';
       resolve({
@@ -44,6 +44,58 @@ var CheckNpmInstall = function CheckNpmInstall() {
   return CheckPackageInstall('npm --version');
 };
 
+var Clone =
+/*#__PURE__*/
+function () {
+  var _ref = _asyncToGenerator(
+  /*#__PURE__*/
+  _regeneratorRuntime.mark(function _callee(gitPath, targetPath) {
+    var name, target;
+    return _regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            name = path.basename(gitPath).split('.')[0];
+            target = path.join(targetPath, name);
+            _context.next = 4;
+            return nodeTool.Path.exists(target);
+
+          case 4:
+            if (!_context.sent) {
+              _context.next = 6;
+              break;
+            }
+
+            return _context.abrupt("return", Exec("cd ".concat(target, " && git fetch --all && git reset --hard origin/master")).then(function (res) {
+              return _objectSpread({}, res, {
+                targetPath: target
+              });
+            }));
+
+          case 6:
+            return _context.abrupt("return", Exec("git clone ".concat(gitPath, " ").concat(target)).then(function (res) {
+              return _objectSpread({}, res, {
+                targetPath: target
+              });
+            }));
+
+          case 7:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee, this);
+  }));
+
+  return function Clone(_x, _x2) {
+    return _ref.apply(this, arguments);
+  };
+}();
+
+var git = {
+  Clone: Clone
+};
+
 /**
  * @file index
  * @author xiaozhihua
@@ -53,4 +105,6 @@ var CheckNpmInstall = function CheckNpmInstall() {
 exports.CheckPackageInstall = CheckPackageInstall;
 exports.CheckYarnInstall = CheckYarnInstall;
 exports.CheckNpmInstall = CheckNpmInstall;
+exports.Shell = Shell;
 exports.Exec = Exec;
+exports.Git = git;
--- dist/shell-tool.esm.js
+++ dist/shell-tool.esm.js
@@ -1,15 +1,15 @@
 import Shell from 'shelljs';
+import _regeneratorRuntime from '@babel/runtime/regenerator';
+import _objectSpread from '@babel/runtime/helpers/objectSpread';
+import _asyncToGenerator from '@babel/runtime/helpers/asyncToGenerator';
+import { basename, join } from 'path';
+import { Path } from 'node-tool';
 
-/**
- * @file shell
- * @author xiaozhihua
- * @date 2018-12-12 09:13:49
- */
-var Exec = function Exec(shell) {
+var Exec = function Exec(shell, options) {
   return new Promise(function (resolve) {
-    Shell.exec(shell, {
+    Shell.exec(shell, _objectSpread({
       silent: true
-    }, function (code) {
+    }, options), function (code) {
       var output = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '';
       var err = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : '';
       resolve({
@@ -38,10 +38,62 @@ var CheckNpmInstall = function CheckNpmInstall() {
   return CheckPackageInstall('npm --version');
 };
 
+var Clone =
+/*#__PURE__*/
+function () {
+  var _ref = _asyncToGenerator(
+  /*#__PURE__*/
+  _regeneratorRuntime.mark(function _callee(gitPath, targetPath) {
+    var name, target;
+    return _regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            name = basename(gitPath).split('.')[0];
+            target = join(targetPath, name);
+            _context.next = 4;
+            return Path.exists(target);
+
+          case 4:
+            if (!_context.sent) {
+              _context.next = 6;
+              break;
+            }
+
+            return _context.abrupt("return", Exec("cd ".concat(target, " && git fetch --all && git reset --hard origin/master")).then(function (res) {
+              return _objectSpread({}, res, {
+                targetPath: target
+              });
+            }));
+
+          case 6:
+            return _context.abrupt("return", Exec("git clone ".concat(gitPath, " ").concat(target)).then(function (res) {
+              return _objectSpread({}, res, {
+                targetPath: target
+              });
+            }));
+
+          case 7:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee, this);
+  }));
+
+  return function Clone(_x, _x2) {
+    return _ref.apply(this, arguments);
+  };
+}();
+
+var git = {
+  Clone: Clone
+};
+
 /**
  * @file index
  * @author xiaozhihua
  * @date 2018-12-11 22:34:43
  */
 
-export { CheckPackageInstall, CheckYarnInstall, CheckNpmInstall, Exec };
+export { CheckPackageInstall, CheckYarnInstall, CheckNpmInstall, Shell, Exec, git as Git };
